bc4529b5dd8c25d837b4711500fd0285808f197b
Kalle Hallivuori <kato@iki.fi>
2016-05-03 23:34:49 +0300

    Extracted Main to its own module covered with tests.

--- test/application_test.py
+++ test/application_test.py
@@ -1,9 +1,30 @@
 import sys
 import unittest
-import subprocess
+from subprocess import Popen, PIPE
 
 
 class ApplicationTest(unittest.TestCase):
 
     def test_start_ok(self):
-        subprocess.check_call([sys.executable, 'datafaser'])
+        stdout, stderr, status = self._call()
+        self.assertEquals(stdout, b'', 'datafaser without arguments produces no standard output')
+        self.assertNotRegexpMatches(stderr, b'ERROR', 'datafaser without arguments produces no error messages')
+        self.assertEquals(status, 0, 'datafaser without arguments exits ok')
+
+    def test_help_ok(self):
+        stdout, stderr, status = self._call('--help')
+        self.assertEquals(stdout, b'', 'Option --help produces no standard output')
+        self.assertRegexpMatches(stderr, b'--help', 'Option --help explains usage')
+        self.assertEqual(status, 1, 'Option --help exits with status 1')
+
+    def test_unrecognized_option_fails(self):
+        stdout, stderr, status = self._call('--unrecognized-option')
+        self.assertEquals(stdout, b'', 'Unrecognized option produces no standard output')
+        self.assertRegexpMatches(stderr, b'--help', 'Unrecognized option explains usage')
+        self.assertEquals(status, 2, 'Unrecognized option exits with status 2')
+
+    def _call(self, *options):
+        full_command = [sys.executable, 'datafaser'] + list(options)
+        process = Popen(full_command, stderr=PIPE, stdout=PIPE)
+        stdout, stderr = process.communicate()
+        return stdout, stderr, process.returncode
--- test/main_test.py
+++ test/main_test.py
@@ -3,9 +3,36 @@ import sys
 import unittest
 import runpy
 from datafaser import operations
+from datafaser.main import Main
+from getopt import GetoptError
+import logging
+import logging.config
+from io import StringIO
 
 
-class MainTest(unittest.TestCase):
+class OutputBlocker:
+
+    _not_set = []
+
+    def block_output(self):
+        self.original_stdout = sys.stdout
+        self.original_stderr = sys.stderr
+        sys.stdout = StringIO()
+        sys.stderr = StringIO()
+        self.original_exit = sys.exit
+        sys.exit = self._exit_called
+        self.exit_status = self._not_set
+
+    def unblock_output(self):
+        sys.stdout = self.original_stdout
+        sys.stderr = self.original_stderr
+        sys.exit = self.original_exit
+
+    def _exit_called(self, status):
+        self.exit_status = status
+
+
+class MetaMainModuleTest(unittest.TestCase, OutputBlocker):
 
     _schema_path_required_for_validation = 'schema.properties.datafaser.properties.run.properties.options.properties'
 
@@ -13,16 +40,22 @@ class MainTest(unittest.TestCase):
         self.original_argv = sys.argv
         self.original_path = sys.path
         self.original_operations_provider = operations.get_default_operations_map
+        self.got_files = None
+        self.block_output()
 
     def tearDown(self):
         sys.argv = self.original_argv
         sys.path = self.original_path
         operations.get_default_operations_map = self.original_operations_provider
+        self.unblock_output()
 
     def test_starts_ok(self):
-        self.got_files = None
         self._run_main()
-        self.assertEqual(sys.argv[1:], self.got_files, 'main calls run with command-line arguments without program name')
+        self.assertEqual(sys.argv[1:], self.got_files, '__main__ runs with command-line arguments without program name')
+
+    def test_unrecognized_option_fails(self):
+        self._run_main(['datafaser', '--bad-option'])
+        self.assertEquals(self.exit_status, 2, '__main__ exits with status 2 on unrecognized option')
 
     def test_fixes_path_ok(self):
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,9 +63,9 @@ class MainTest(unittest.TestCase):
         self._run_main()
         self.assertIn(base_dir, sys.path, 'main adds datafaser to path')
 
-    def _run_main(self):
+    def _run_main(self, args=None):
         operations.get_default_operations_map = self._mock_operations_map
-        sys.argv = ['some', 'strings']
+        sys.argv = args or ['some', 'strings']
         runpy.run_module('datafaser', run_name='__main__')
 
     def _mock_operations_map(self, _):
@@ -44,3 +77,74 @@ class MainTest(unittest.TestCase):
 
     def _create_schema_required_for_validation(self, data_tree):
         data_tree.reach(self._schema_path_required_for_validation, create_containers=True)
+
+
+class MainObjectTest(unittest.TestCase, OutputBlocker):
+
+    def setUp(self):
+        self.original_dictConfig = logging.config.dictConfig
+        logging.config.dictConfig = self._config_called
+        self.config = self._not_set
+        self.block_output()
+
+    def tearDown(self):
+        logging.config.dictConfig = self.original_dictConfig
+        self.unblock_output()
+
+    def test_help_ok(self):
+        Main(['datafaser', '--help']).run_with_command_line()
+        self.assertEquals(self.exit_status, 1, 'Option --help exits with status 1')
+
+    def test_no_logging_ok(self):
+        Main(['datafaser']).run_with_command_line()
+        self.assertIs(self.exit_status, self._not_set, 'Datafaser without options does not call exit')
+        self.assertIsInstance(self.config, dict, 'Datafaser without options configures logging')
+        self.assertIs(
+                self.config.get('handlers', {}).get('syslog', self._not_set),
+                self._not_set,
+                'Datafaser without options does not configure syslog'
+        )
+        self.assertIs(
+                self.config.get('handlers', {}).get('file', self._not_set),
+                self._not_set,
+                'Datafaser without options does not configure log file'
+        )
+
+    def test_unrecognized_option_fails(self):
+        exception = None
+        try:
+            Main(['datafaser', '--unrecognized-option']).run_with_command_line()
+        except GetoptError as e:
+            exception = e
+        self.assertIsNotNone(exception, 'Unrecognized option raises exception')
+
+    def test_log_file_option_ok(self):
+        Main(['datafaser', '--log-file', '-']).run_with_command_line()
+        self.assertIsInstance(self.config, dict, 'Option --log-file configures logging')
+        self.assertEquals(
+                self.config.get('handlers', {}).get('file', {}).get('filename'),
+                '-',
+                'Option --log-file sets log filename'
+        )
+        self.assertIs(
+                self.config.get('handlers', {}).get('syslog', self._not_set),
+                self._not_set,
+                'Option --log-file not configure syslog'
+        )
+
+    def test_syslog_option_ok(self):
+        Main(['datafaser', '--syslog']).run_with_command_line()
+        self.assertIsInstance(self.config, dict, 'Option --syslog configures logging')
+        self.assertEquals(
+                self.config.get('handlers', {}).get('syslog', {}).get('()'),
+                'logging.handlers.SysLogHandler',
+                'Option --syslog defines syslog handler'
+        )
+        self.assertIs(
+                self.config.get('handlers', {}).get('file', self._not_set),
+                self._not_set,
+                'Option --syslog does not configure log file'
+        )
+
+    def _config_called(self, config):
+        self.config = config
