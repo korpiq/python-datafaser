b30ec4ddf4b746cd042f722060fe31ae17ce176d
Kalle Hallivuori <korpiq@iki.fi>
2016-11-23 22:14:12 +0200

    Cleaner output call in save test

--- test/files/save_test.py
+++ test/files/save_test.py
@@ -16,19 +16,22 @@ class FileWriterStdoutTest(unittest.TestCase):
         sys.stdout = self._stdout
 
     def test_write_yaml_to_stdout_ok(self):
-        FileSaver({'foo': ['bar']}, FormatRegister(**default_settings)).save('-', 'yaml')
+        self.__output_as({'foo': ['bar']}, 'yaml')
         expected = 'foo:\n- bar\n'
         self.assertEqual(expected, self.buffer.getvalue(), 'Yaml written in expected format to STDOUT')
 
     def test_write_json_to_stdout_ok(self):
-        FileSaver({'foo': ['bar']}, FormatRegister(**default_settings)).save('-', 'json')
+        self.__output_as({'foo': ['bar']}, 'json')
         expected = '{\n    "foo": [\n        "bar"\n    ]\n}\n'
         self.assertEqual(expected, self.buffer.getvalue(), 'Json written in expected format to STDOUT')
 
     def test_write_text_to_stdout_ok(self):
         expected = 'Hello there!'
-        FileSaver(expected, FormatRegister(**default_settings)).save('-', 'text')
+        self.__output_as(expected, 'text')
         self.assertEqual(expected, self.buffer.getvalue(), 'Text written in expected format to STDOUT')
+
+    def __output_as(self, data, output_type):
+        return FileSaver(data, FormatRegister(**default_settings)).save('-', output_type)
 
     def test_write_dict_as_text_to_stdout_fails(self):
         exception = None
